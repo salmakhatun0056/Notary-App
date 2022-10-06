23b6fa3ca50f30eb231c819aa4b423399bb90128
salmakhatun12345 <ummesalma0056@gmail.com>
2022-10-06 23:53:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    const postLoad = async () => {
+      const res = await axios.post('http://demo2211087.mockable.io/mock')
+      setPost(res?.data)
+    }
+    postLoad();
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overflow-x-auto relative shadow-md sm:rounded-lg w-4/5 lg:w-1/2 mx-auto mt-12">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <tbody>
+          {post?.companies?.map((user, i) => (
+            <tr key={i} className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-600">
+              <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {user.name}
+              </th>
+              <td className="py-4 px-6">
+                {user.email}
+              </td>
+              <td className="py-4 px-2 ">
+                <span className="tracking-wider justify-center items-center gap-x-1 flex border text-black px-2 border-rounded py-1 text-sm rounded leading-loose  font-semibold" title="">
+                  <i className="fas fa-circle block text-blue-700 " aria-hidden="true"></i> {user.status}
+                </span>
+              </td>
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
     </div>
   );
 }
